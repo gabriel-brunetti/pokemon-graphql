fe4440401ef9e17e6276eff4ea17f62eace446ba
Gabriel Brunetti <gabriel.brunetti1@gmail.com>
2021-07-16 14:24:53 -0300

    Updates

---
+++ precache-manifest.a837fd642559e0d798952df29e53b641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fec6406d1440ef9639b6021a8bb7e78",
+    "url": "/pokemon-graphql/index.html"
+  },
+  {
+    "revision": "8bfb3c01302a86dde8a5",
+    "url": "/pokemon-graphql/static/css/main.2fcf9d41.chunk.css"
+  },
+  {
+    "revision": "c314c1baa747e3a97593",
+    "url": "/pokemon-graphql/static/js/2.307ff5ce.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/pokemon-graphql/static/js/2.307ff5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfb3c01302a86dde8a5",
+    "url": "/pokemon-graphql/static/js/main.ce1a9702.chunk.js"
+  },
+  {
+    "revision": "1f621ca588d18a8bc321",
+    "url": "/pokemon-graphql/static/js/runtime-main.a6649373.js"
+  }
+]);
